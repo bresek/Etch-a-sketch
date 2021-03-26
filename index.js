c6baee301ed5832873d594969b0786629f6699b6
BENJAMIN RESEK <bresek@clarku.edu>
2021-03-25 21:16:25 -0400

    added button that changes canvas size changed color from black to random color function

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ console.log("script connected")
 
 
 const container = document.querySelector('.container')
-grid()
 
-function grid(i = 16, j = 16){
-  for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
+grid()
+sketch()
+function grid(rows = 16, cols = 16){
+  document.querySelector('.container').style.gridTemplateColumns = `repeat(${rows}, 1fr)`
+  for (let i = 0; i < rows; i++){
+    for (let j = 0; j < cols; j++){
       const div = document.createElement('div')
       div.classList.add("pixel")
-      div.textContent = "#"
+      div.textContent = ""
       container.appendChild(div)
     }
 
@@ -17,10 +19,36 @@ function grid(i = 16, j = 16){
 }
 
 
+const btn = document.querySelector("button")
 
-const pixels = document.querySelectorAll(".pixel")
-pixels.forEach((pixel)=>{
-  pixel.addEventListener("mouseenter",()=>{
-    pixel.style.cssText = "background-color: black"
-  })
+btn.addEventListener("click", ()=>{
+  container.innerHTML ='' //erase last grid
+  while (true){
+    side = prompt("how many squares per side for your new grid?")
+    if (side > 100){
+      alert('enter a value under 100')
+    }else{
+      break
+    }
+
+  }
+
+  grid(side,side)
+  sketch()//had to include in here because sketch stopped working after clearing
 })
+
+
+function sketch(){
+  const pixels = document.querySelectorAll(".pixel")
+  pixels.forEach((pixel)=>{
+    pixel.addEventListener("mouseenter",()=>{
+      pixel.style.backgroundColor = randColor()
+    })
+  })
+}
+
+function randColor(){
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  randomColor = '#'+randomColor
+  return randomColor
+}
